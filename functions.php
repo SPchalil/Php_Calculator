39c10863e161fd9f23e4b48d4784f35bae790101
Swapna Chalil <74065235+SPchalil@users.noreply.github.com>
2021-07-27 15:34:22 -0230

    -

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+    function myCalculator($num1, $oper, $num2){
+       $sum;
+       $favcolor = "red";
+
+switch ($oper) {
+  case "add":
+    $sum = $num1 + $num2;
+    
+    break;
+  case "sub":
+    $sum = $num1 - $num2;
+    break;
+  default:
+    $sum = "Error";
+    break;
+    } 
+    return $sum;
+    }
+
+    $num1 = $_GET["num1"];
+    $num2 = $_GET["num2"];
+    $oper = $_GET["oper"];
+
+    echo "Value : ". myCalculator($num1, $oper, $num2);
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <form action="functions.php" method="get">
+            <input type="text" name="num1" placeholder="Number 1">
+            <select name ='oper'>
+                <label>Choose operation!</label>
+                <option value = "add">Add</option>
+                <option value = "sub">Substract</option>
+            </select>
+            <input type="text" name="num2" placeholder="Number 2">
+            <button type = "submit"> Calculate!</button>
+        </form>
+        
+    </body>
+</html>
